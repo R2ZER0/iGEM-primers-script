8c159a07b1767965f15880edda80ee4c1edaf4c0
Rikki Guy <git@rikkiguy.me.uk>
2014-07-04 15:45:45 +0100

    more changes

--- primers.py
+++ primers.py
@@ -1,9 +1,11 @@
 
 # How to use: copy and paste these two bits of text into a program called IDLE (search for it on the uni system)
 # Each needs to be in front of arrows (>>>) which tell you where IDLE starts
-# then type (no quote marks) "primer('sequence goes here')". Press enter.
+# then type "primer('sequence goes here')". Press enter.
 # currently it doesn't put DR and UR on the same line as their prefixes and suffixes. Not sure why
+# To confirm the primers fit where they should, use ApE’s alignment tool
 
+ 
 
 
 def rc(sequence, n, b):
@@ -33,11 +35,17 @@ def rc(sequence, n, b):
 
 def primers(s):
     print 'UF is CTAgag' + s[0:37] #print first 37 bases with the prefix
-    print 'UR is (add ctc at end)' #reverses the first 34 bases of the primer
-    rc(s, 0, 35)
-    if s[-37:-35] == 'tag' or 'cta':  #takes last 37 bases, checks first 3 for stops  
+        print 'UR is (add ctc at end)' #reverses the first 34 bases of the primer
+    rc(s, 0, 34)
+    ## if s[-37:-35] == 'tag' or 'cta': 
+    ## ^ easy thing to be caught out on, this actually means:
+    ##   if (s[-37:-35] == ‘tag’) or (‘cta’): 
+    ## ‘or’ works solely with boolean logic, and in python all non-empty strings
+    ##  are True (noidea why), so this if statement can never fail!
+    
+    if (s[-37:-34] == 'tag') or (s[-37:-34] == 'cta'):  #takes last 37 bases, checks first 3 for stops  
         print 'bad codon found'
-        if s[-36:-34] == 'tag':
+        if s[-36:-33] == 'tag':
             print 'bad codon found again'
             print 'DF is ta' + s[-35:]
             print 'DR is TAGta'
@@ -51,4 +59,6 @@ def primers(s):
     else:
         print 'codons fine, DF is ta' + s[-37:]
         print 'DR is TAGta'
-        rc(s, -37, -3)
+        rc(s, -37, -3) # this will take DF, take 3 from the end, and produce its complement. 
+#ALso cool would be to get it run batches of sequences, and, most helpfully, remove the whitespace from sequences copied and pasted from the registry, because that’s a hassle
+
