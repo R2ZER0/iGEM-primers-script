b387d83a238f0dbfd047369239b19e9cc8ea811d
Rikki Guy <git@rikkiguy.me.uk>
2014-07-07 18:53:43 +0100

    A better way of GUIing

--- test.py
+++ test.py
@@ -8,21 +8,29 @@ from primers import primers
 # Biopython's sequence import library
 from Bio import SeqIO
 
-# Qt's stuff
+# Qt's GUI stuff
 import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
-from PySide.QtDeclarative import QDeclarativeView
+from PySide.QtDeclarative import *
+
+class MainWindow(QDeclarativeView):
+    
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setWindowTitle("EdiGEM 2014: Primers")
+        self.setSource(QUrl.fromLocalFile("view.qml"))
+        self.setResizeMode(QDeclarativeView.SizeRootObjectToView)
+    
+if __name__ == '__main__':
+    
+    # Create a new GUI application
+    application = QApplication(sys.argv)
+    
+    # Display our main window
+    window = MainWindow()
+    window.show()
+    
+    # Run, be free!
+    sys.exit(application.exec_())
  
-# Create Qt application and the QDeclarative view
-app = QApplication(sys.argv)
-view = QDeclarativeView()
-# Create an URL to the QML file
-url = QUrl('view.qml')
-# Set the QML file and show
-view.setSource(url)
-view.show()
-# Enter Qt main loop
-sys.exit(app.exec_())
-
-
